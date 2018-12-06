455491525b55aade9d20fd80bb72c94a9c4eb865
roman <main@roman.com>
2018-12-06 15:47:52 +0100

    fixed

--- download.php
+++ download.php
@@ -11,9 +11,8 @@ if (count($argv)!=3) {
 
 $file = $argv[1];
 $temp_folder =  $argv[2];
-
 $outfile = $temp_folder.basename($file);
-array_map('unlink', glob($temp_folder."*"));
+$extracted = str_replace('.gz', '', basename($file));
 
 file_put_contents($outfile, fopen($file, 'r') );
 
@@ -25,8 +24,9 @@ if (file_exists($outfile)) {
 }
 exec("gzip -d $outfile");
 
-$files = scandir($temp_folder);
-if (!isset($files[2])){
-	echo 'no files'; die();
+
+if (file_exists($temp_folder.$extracted)) {
+    echo PHP_EOL."extracted:".$temp_folder.$extracted.PHP_EOL;
+} else {
+    echo PHP_EOL."NOT extracted!";
 }
-echo PHP_EOL."extracted:".$temp_folder.$files[2].PHP_EOL;
