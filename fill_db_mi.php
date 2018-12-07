a13f4c8065d76ce52a45e397efb3eacdfd97a7dd
Roman <Roman@rr.ru>
2018-12-07 05:10:43 -0800

    added ni

---
+++ fill_db_mi.php
@@ -0,0 +1,38 @@
+<?php 
+
+set_time_limit(0);
+
+/* SCRIPT SETTINGS */
+$host = 'localhost'; // DB HOST
+$dbname = 'testdb2'; //DB NAME
+
+$user = 'root'; // DB USER
+$pass = ''; // DB PASSWORD
+
+$dsn = 'mysql:host='.$host;
+$pdo = new PDO($dsn, $user, $pass, array(PDO::MYSQL_ATTR_LOCAL_INFILE => true));
+
+$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+
+if (count($argv)!=2) {
+    echo('run: php fill_db.php /path/to/csv/file.csv'.PHP_EOL);
+    die();
+}
+$csv_file = $argv[1];
+
+$table_name = str_replace('.csv','',basename($argv[1]));
+
+
+$sth = $pdo->prepare("CREATE DATABASE IF NOT EXISTS $dbname");
+$sth->execute();
+
+//create table if exist
+$create_table = 'CREATE TABLE IF NOT EXISTS '.$dbname.'.'.$table_name.' (domain text NOT NULL,  first_seen int(11) NOT NULL, last_seen int(11)NOT NULL, etld text NOT NULL, id int(11) NOT NULL,time_date_imported TIMESTAMP DEFAULT CURRENT_TIMESTAMP, primary key (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;';
+
+$sth = $pdo->prepare($create_table);
+$sth->execute();
+
+exec("mysqlimport --ignore-lines=1 --fields-terminated-by=, --verbose --local -u".$user." -p".$pass." ".$dbname." ".$csv_file);
+
+echo PHP_EOL."Done!".PHP_EOL;
+?>
